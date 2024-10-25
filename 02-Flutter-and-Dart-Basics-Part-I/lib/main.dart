cf9eeee2ff9eb92933f2cd2ab11f59c4a599ec61
Rush-2K <syaidiazhari@gmail.com>
2024-10-25 19:51:46 +0800

    build custom widgets

--- 02-Flutter-and-Dart-Basics-Part-I/lib/main.dart
+++ 02-Flutter-and-Dart-Basics-Part-I/lib/main.dart
@@ -4,25 +4,34 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        body: Container(
+        body: GradientContainer(),
+      ),
+    ),
+  );
+}
+
+class GradientContainer extends StatelessWidget {
+  @override
+  Widget build(context) {
+    return Container(
           decoration: const BoxDecoration(
             gradient: LinearGradient(
               colors: [
-                Color.fromARGB(255, 153, 10, 189),
-                Color.fromARGB(255, 183, 141, 194),
+                Color.fromARGB(255, 7, 24, 172),
+                Color.fromARGB(255, 122, 142, 185),
               ],
               begin: Alignment.topLeft,
               end: Alignment.bottomRight,
             ),
           ),
           child: const Center(
-            child: Text('Welcome Siri', style: TextStyle(
-              color: Colors.white,
-              fontSize: 28.0,
-            )),
+            child: Text(
+              'Hello World!',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 28.0,
+                )),
           ),
-        ),
-      ),
-    ),
-  );
+        );
+  }
 }
