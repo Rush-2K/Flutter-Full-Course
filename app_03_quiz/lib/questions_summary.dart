33a4639d8cff830e899af655b8fba6ba29017ad4
Rush-2K <syaidiazhari@gmail.com>
2024-10-31 13:51:31 +0800

    add alignment for result screen, change color based on question answered correctly, use container to produce shape

--- app_03_quiz/lib/questions_summary.dart
+++ app_03_quiz/lib/questions_summary.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:google_fonts/google_fonts.dart';
 
 class QuestionSummary extends StatelessWidget {
   const QuestionSummary(this.summaryData, {super.key});
@@ -8,24 +8,68 @@ class QuestionSummary extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
     return SizedBox(
-      height: 300,
+      height: 400,
       child: SingleChildScrollView(
         child: Column(
           children: summaryData.map(
             (data) {
+              final isCorrectAnswer = data['user_answer'] == data['correct_answer'];
               return Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text(((data['question_index'] as int) + 1).toString()),
+                  Container(
+                    width: 30,
+                    height: 30,
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                      color: isCorrectAnswer ? const Color.fromARGB(255, 150, 198, 241) : const Color.fromARGB(255, 249, 133, 241),
+                      borderRadius: BorderRadius.circular(100),
+                      border: Border.all(
+                        color: Colors.red,
+                      ),
+                    ),
+                    
+                    child: Text(
+                      ((data['question_index'] as int) + 1).toString(),
+                      style: GoogleFonts.lato(
+                        color: const Color.fromARGB(255, 255, 255, 255),
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 30,
+                  ),
                   Expanded(
                     child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Text(data['question'] as String),
+                        Text(
+                          data['question'] as String,
+                          style: GoogleFonts.lato(
+                            color: const Color.fromARGB(255, 255, 255, 255),
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
                         const SizedBox(
                           height: 5,
                         ),
-                        Text(data['user_answer'] as String),
-                        Text(data['correct_answer'] as String)
+                        Text(
+                          data['user_answer'] as String,
+                          style: const TextStyle(
+                            color: Color.fromARGB(255, 202, 171, 252),
+                          ),
+                        ),
+                        Text(
+                          data['correct_answer'] as String,
+                          style: const TextStyle(
+                            color: Color.fromARGB(255, 181, 254, 246),
+                          ),
+                        )
                       ],
                     ),
                   ),
--- app_03_quiz/lib/results_screen.dart
+++ app_03_quiz/lib/results_screen.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:app_03_quiz/data/questions.dart';
 import 'package:app_03_quiz/questions_summary.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class ResultsScreens extends StatelessWidget {
-  const ResultsScreens({super.key, required this.chosenAnswers});
+  const ResultsScreens({
+    super.key,
+    required this.chosenAnswers,
+    required this.onRestart,
+  });
 
+  final void Function() onRestart;
   final List<String> chosenAnswers;
 
   List<Map<String, Object>> getSummaryData() {
@@ -39,6 +45,12 @@ class ResultsScreens extends StatelessWidget {
           children: [
             Text(
               'You answered $numCorrectQuestions out of $numTotalQuestions questions correctly!',
+              style: GoogleFonts.lato(
+                color: const Color.fromARGB(255, 230, 200, 253),
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
             ),
             const SizedBox(
               height: 30,
@@ -47,9 +59,13 @@ class ResultsScreens extends StatelessWidget {
             const SizedBox(
               height: 30,
             ),
-            TextButton(
-              onPressed: () {},
-              child: const Text('Restart Quiz!'),
+            TextButton.icon(
+              icon: const Icon(Icons.refresh),
+              onPressed: onRestart,
+              label: const Text('Restart Quiz!'),
+              style: TextButton.styleFrom(
+                foregroundColor: Colors.white,
+              ),
             )
           ],
         ),
