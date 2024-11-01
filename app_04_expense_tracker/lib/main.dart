a6fee6a06e4f334d91e840714f5163bcc4e5c9ba
Rush-2K <syaidiazhari@gmail.com>
2024-11-01 23:24:04 +0800

    organize back the folder, create separate item UI, add intl third pty package to format date

--- app_04_expense_tracker/lib/main.dart
+++ app_04_expense_tracker/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'package:app_04_expense_tracker/expenses.dart';
+import 'package:app_04_expense_tracker/widgets/expenses.dart';
 
 void main() {
   runApp(
