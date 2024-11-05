5b9f3d3943d3aa791c7a95f7a009edde73d257fb
Rush-2K <syaidiazhari@gmail.com>
2024-11-06 00:02:52 +0800

    update flutter to latest version, manage folder, display category items, make widget tappable

--- app_06_meals/lib/main.dart
+++ app_06_meals/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:app_06_meals/screens/categories.dart';
 import 'package:flutter/material.dart';
 
 import 'package:google_fonts/google_fonts.dart';
@@ -22,7 +23,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: theme,
-      home: // Todo ...,
+      home: const CategoriesScreen(),
     );
   }
 }
